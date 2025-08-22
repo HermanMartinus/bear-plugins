2c065217fe436af5252a4d2accc62542ee8ac6ff
Herman Martinus <hfbmartinus@gmail.com>
2025-08-22 09:28:40 +0200

    update: visual editor using overtype

---
+++ plugins/visual-editor.js
@@ -0,0 +1,41 @@
+// Ensure you add the import for the Overtype library above this one
+// <script src="https://unpkg.com/overtype"></script>
+
+if ($textarea) {
+    $textarea.style.display = "none";
+    $uploadButton.style.display = "none"; // This is incompatible with the editor
+
+    // Initialize demo editors
+    document.addEventListener('DOMContentLoaded', function() {
+
+        if ($textarea) {
+            $overtypeDiv = document.createElement('div');
+            $overtypeDiv.className = 'overtype-editor';
+            $overtypeDiv.style.height = '700px';
+            $textarea.insertAdjacentElement('afterend', $overtypeDiv);
+        }
+
+        const OT = window.OverType.default || window.OverType;
+        // Initialize all toolbar editors
+        new OT('.overtype-editor', {
+            value: $textarea.value,
+            toolbar: true,
+            theme: 'cave',
+            showStats: true,
+            placeholder: '...',
+            onChange: (value, instance) => {
+                $textarea.value = value;
+            }
+        });
+
+        $overtypeTextarea = document.querySelector('.overtype-input');
+        
+        $overtypeTextarea.scrollTop = sessionStorage.getItem('overtypeEditorY') || 0
+        $overtypeTextarea.addEventListener("scroll", function(e){
+            sessionStorage.setItem('overtypeEditorY', $overtypeTextarea.scrollTop)
+        })
+    });
+}
+
+
+
