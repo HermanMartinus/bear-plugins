5fe29fa6dd2e8b9c0fcbd7138157e626fed4a679
Reed <8450080+ReedyBear@users.noreply.github.com>
2025-10-30 15:11:32 -0500

    Updated docblock, add some comments to code body

--- plugins/newline-fixer.js
+++ plugins/newline-fixer.js
@@ -1,5 +1,5 @@
 /**
-* Ensures each line in your post has two spaces at the end. Markdown requires either a blank line between paragraphs or two spaces at the end of a line in order to render as separate paragraphs.
+* Ensures each line in your post has two spaces at the end, so that markdown renders new lines.
 *
 * change `const autofix = true;` to `const autofix = false;` to add a 'Fix new lines' button instead of fixing automatically on submission.
 * Created by ReedyBear, see https://reedybear.bearblog.dev/bearblog/ for other creations.
@@ -15,6 +15,7 @@ document.addEventListener('DOMContentLoaded', function() {
       return; // stop execution of we're not on a new post page
   }
   if (autofix){
+    // update the submit buttons so the fix is applied automatically when they are clicked
     const submit_buttons = document.querySelectorAll('.sticky-controls > button[type="submit"]');
     for (const button of submit_buttons){
         button.setAttribute('onclick', 
@@ -23,7 +24,7 @@ document.addEventListener('DOMContentLoaded', function() {
             );
     }
   } else {
-    // create a new button element
+    // create a new button, which you can click to apply the new lines fix
     const btn = document.createElement('button');
     btn.classList.add('markdown_line_fixer');
     btn.setAttribute('onclick', "event.preventDefault();fix_new_lines(event);");
@@ -33,11 +34,13 @@ document.addEventListener('DOMContentLoaded', function() {
   }
 });
 
-
+/** ensures every line in the post content has two spaces at the end */
 function fix_new_lines(event){
   const body_node = document.querySelector('#body_content');
   const body = body_node.value;
+  // regex finds lines that have text on them and do not end with 2 spaces.
   const regex = /([^\n ])( {0,1})\n/g;
+  // The $1 makes sure that the last character of each line does not get deleted.
   const fixed_body = body.replaceAll(regex, "$1  \n");
   body_node.value = fixed_body;
 }
