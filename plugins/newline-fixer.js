b4efe90607e94b4c53f95cb43f5b8c6587125924
Reed <8450080+ReedyBear@users.noreply.github.com>
2025-11-04 15:15:43 -0600

    Allow plugin to function if DOMContentLoaded has already triggered

--- plugins/newline-fixer.js
+++ plugins/newline-fixer.js
@@ -5,34 +5,39 @@
 * Created by ReedyBear, see https://reedybear.bearblog.dev/bearblog/ for other creations.
 */
 
-document.addEventListener('DOMContentLoaded', function() {
-  // when 'autofix = false;', a button will be added to your 'New Post' page. When 'autofix = true', the fix will be added automatically on-submission.
-  const autofix = true;
-
-  // check if we're on a page with a new post form
-  const form = document.querySelector('form.post-form');
-  if (form == null) {
-      return; // stop execution of we're not on a new post page
-  }
-  if (autofix){
-    // update the submit buttons so the fix is applied automatically when they are clicked
-    const submit_buttons = document.querySelectorAll('.sticky-controls > button[type="submit"]');
-    for (const button of submit_buttons){
-        button.setAttribute('onclick', 
-            button.getAttribute('onclick')
-            +";fix_new_lines(event);"
-            );
+(document.readyState === "loading" 
+    ? document.addEventListener.bind(this,'DOMContentLoaded')  
+    : function(f){f();}.bind(this)
+).call(this,
+  function() {
+    // when 'autofix = false;', a button will be added to your 'New Post' page. When 'autofix = true', the fix will be added automatically on-submission.
+    const autofix = true;
+  
+    // check if we're on a page with a new post form
+    const form = document.querySelector('form.post-form');
+    if (form == null) {
+        return; // stop execution of we're not on a new post page
     }
-  } else {
-    // create a new button, which you can click to apply the new lines fix
-    const btn = document.createElement('button');
-    btn.classList.add('markdown_line_fixer');
-    btn.setAttribute('onclick', "event.preventDefault();fix_new_lines(event);");
-    btn.innerText = 'Fix new lines';
-    // add the button to your 'New Post' page
-    document.querySelector('.sticky-controls').appendChild(btn);
+    if (autofix){
+      // update the submit buttons so the fix is applied automatically when they are clicked
+      const submit_buttons = document.querySelectorAll('.sticky-controls > button[type="submit"]');
+      for (const button of submit_buttons){
+          button.setAttribute('onclick', 
+              button.getAttribute('onclick')
+              +";fix_new_lines(event);"
+              );
+      }
+    } else {
+      // create a new button, which you can click to apply the new lines fix
+      const btn = document.createElement('button');
+      btn.classList.add('markdown_line_fixer');
+      btn.setAttribute('onclick', "event.preventDefault();fix_new_lines(event);");
+      btn.innerText = 'Fix new lines';
+      // add the button to your 'New Post' page
+      document.querySelector('.sticky-controls').appendChild(btn);
+    }
   }
-});
+);
 
 /** ensures every line in the post content has two spaces at the end */
 function fix_new_lines(event){
